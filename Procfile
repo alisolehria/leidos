21dfeb21fed24a7063edf43fe8c5ca6e06ecf55e
Solehria\alisolehria <alimuzaffar@live.com>
2017-01-24 22:51:13 +0400

    Added MySQL Server Database

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn leidos.wsgi --log-file -
--- leidos/settings.py
+++ leidos/settings.py
@@ -134,9 +134,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'leidos.syntax@gmail.com'
