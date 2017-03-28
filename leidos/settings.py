14b0b1edd0d89bc08bd5035624336056f2d743f1
Solehria\alisolehria <alimuzaffar@live.com>
2017-03-28 21:32:07 +0400

    bug fixes

--- leidos/settings.py
+++ leidos/settings.py
@@ -155,6 +155,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
+SECURE_SSL_REDIRECT = False
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
